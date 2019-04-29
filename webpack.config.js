0acac76a725a922979cbff24207c71abcc3d127c
Danice123 <mitchelldanice@gmail.com>
2019-04-29 11:27:30 -0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: './src/app.js'
+	},
+	output: {
+		filename: '[name].[contenthash].bundle'
+	},
+	mode: 'development',
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Javamon',
+			chunks: ['app']
+		})
+	]
+
+};
