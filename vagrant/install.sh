f9a845930882badcb3ad69c72be2ab850fefa550
Danice123 <mitchelldanice@gmail.com>
2019-04-30 09:58:30 -0400

    vagrant setup

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo "Installing Apache"
 sudo yum install httpd24 -y -q
-sleep 1
-sudo cp /vagrant/vagrant/web.conf /etc/httpd/conf.d/
--- vagrant/start.sh
+++ vagrant/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo cp /vagrant/vagrant/web.conf /etc/httpd/conf.d/
 sudo service httpd start
